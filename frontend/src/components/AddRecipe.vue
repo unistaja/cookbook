b16a19eaa9bf476cb1ea62803995cb6975e92fff
Kairi <kairi_kangro@hotmail.com>
2017-01-21 00:14:40 +0200

    Add some design to recipe input screen

--- frontend/src/components/AddRecipe.vue
+++ frontend/src/components/AddRecipe.vue
@@ -1,37 +1,56 @@
 <template>
   <div id="addrecipe">
-    <div>
-      <input type="text" v-model="recipe.name" placeholder="Pealkiri"/>
-      <input type="text" v-model="recipe.source" placeholder="Allikas"/>
+    <div id="recipeheader">
+      <input id="title" type="text" v-model="recipe.name" placeholder="Pealkiri"/>
+      <input id="source" type="text" v-model="recipe.source" placeholder="Allikas"/>
     </div>
-    <div>
-      <div v-for="list in recipe.ingredientLists">
-        <input type="text" v-model="list.name" placeholder="Jaotise nimi"/><a @click="deleteList(list)">Kustuta</a>
-        <div v-for="line in list.ingredientLines">
-          <input type="number" v-model="line.amount" placeholder="Kogus"/>
-          <input type="text" v-model="line.unit" placeholder="Ühik"/>
-          <input type="text" v-model="line.ingredient" placeholder="Koostisosa"/>
-          <a @click="deleteRow(line, list)">Kustuta</a>
-        </div>
-        <a @click="addRow(list)">Lisa rida</a>
+
+    <div id="pictureholder">
+      <div id="picture">
+        <img src="http://placehold.it/350x350"/>
       </div>
-      <a @click="addList()">Lisa jaotis</a>
+
+      <div id="categories">
+        <div v-for="category in recipe.categories">
+          <input type="text" v-model="category.name" placeholder="Kategooria"/>
+          <a @click="deleteCategoryRow(category)" class="one-char-button delete-button">-</a>
+        </div>
+        <div>
+          <a @click="addCategoryRow()" class="one-char-button add-button">+</a>
+        </div>
+      </div>
     </div>
-    <div>
+
+
+    <div id="ingredients">
+      <div id="ingredient-list">
+        <div v-for="list in recipe.ingredientLists">
+          <input type="text" v-model="list.name" placeholder="Jaotise nimi"/>
+          <a class="one-char-button delete-button" @click="deleteList(list)">-</a>
+          <ul v-for="line in list.ingredientLines">
+            <li><input class="amount-input" type="number" v-model="line.amount" placeholder="Kogus"/>
+            <input class="unit-input" type="text" v-model="line.unit" placeholder="Ühik"/>
+            <input class="ingredient-input" type="text" v-model="line.ingredient" placeholder="Koostisosa"/>
+            <a class="one-char-button delete-button" @click="deleteRow(line, list)">-</a>
+          </ul>
+          <ul>
+            <li><a @click="addRow(list)" class="one-char-button add-button">+</a>
+          </ul>
+        </div>
+        <a @click="addList()" class="one-char-button add-button">+</a>
+      </div>
+    </div>
+    <div id="instructions">
       <textarea v-model="recipe.instructions" placeholder="Juhised" ></textarea>
     </div>
 
-    <div>
-      <div v-for="category in recipe.categories">
-        <input type="text" v-model="category.name" placeholder="Kategooria"/>
-        <a @click="deleteCategoryRow(category)">Kustuta</a>
-      </div>
-      <a @click="addCategoryRow()">Lisa kategooria</a>
-    </div>
-
-    <div>
+    <div id="savebutton">
       <a id="save" @click="saveRecipe()">Salvesta muudatused</a>
     </div>
+
+
+
+
     <!--<a @click="logData()">Logi</a>-->
 
 
@@ -39,35 +58,160 @@
 </template>
 <style scoped>
   #addrecipe {
-    text-align: left;
-  }
-  div {
-    display: block;
+    display:flex;
+    flex-flow: row wrap;
+    align-items: stretch;
   }
 
-  a {
-    display: inline-block;
+  #recipeheader, #ingredients, #instructions, #pictureholder, #savebutton {
+    flex: 1 100%;
+    margin: 10px;
   }
+
+  #recipeheader {
+    order: 0;
+  }
+
+  #savebutton {
+    order: 4;
+  }
+
+  #recipeheader {
+    flex-flow: column;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  #ingredients {
+    flex-flow: column;
+    display:flex;
+    align-items: center;
+  }
+
+  #instructions {
+    flex-flow:column;
+    align-items: center;
+    display: flex;
+  }
+
+  #pictureholder {
+    flex-flow:column;
+    align-items: center;
+    display: flex;
+  }
+
+  textarea {
+    flex: 1;
+    width: 100%;
+  }
+
+  #title {
+    font-size: 1.5em;
+    width: 20em;
+  }
+  #source {
+    width: 20em;
+    margin-top: 5px;
+  }
+
+  .amount-input {
+    width: 4em;
+  }
+
+  .unit-input {
+    width: 3em;
+  }
+
+  .ingredient-input {
+    width: 15em;
+  }
+
+  #categories {
+    display: flex;
+    text-align:left;
+    flex-flow:column;
+  }
+
+  #categories > div {
+    margin: 5px;
+  }
+
+  @media all and (min-width: 825px) {
+    #pictureholder, #ingredients {
+      flex: 1 auto;
+    }
+
+    #ingredients {
+      order: 1;
+    }
+
+    #pictureholder {
+      order: 2;
+    }
+
+    #instructions {
+      order: 3;
+    }
+  }
+
+  @media all and (min-width: 1200px) {
+    #instructions {
+      flex: 6 0px;
+    }
+
+    #ingredients {
+      order: 1;
+    }
+
+    #pictureholder {
+      order: 3;
+    }
+
+    #instructions {
+      order: 2;
+    }
+  }
+
+  #ingredient-list {
+    text-align: left;
+    width: 25em;
+  }
+
+  .one-char-button {
+    font-weight: bold;
+    color: #FFF;
+    padding: 0.1em 0.5em;
+    border-radius: 4px;
+    text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.4);
+    box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.4), 0 1px 1px rgba(0, 0, 0, 0.2);
+    transition-duration: 0.2s;
+    user-select:none;
+  }
+
+  .delete-button {
+    background: #990000;
+    border: solid 1px #20538D;
+  }
+
+  .add-button {
+    background: #009900;
+    border: solid 1px #20538D;
+  }
+
 
   #save {
     padding: 10px 15px;
     background: #4479BA;
     color: #FFF;
-    -webkit-border-radius: 4px;
-    -moz-border-radius: 4px;
     border-radius: 4px;
     border: solid 1px #20538D;
     text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.4);
-    -webkit-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.4), 0 1px 1px rgba(0, 0, 0, 0.2);
-    -moz-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.4), 0 1px 1px rgba(0, 0, 0, 0.2);
     box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.4), 0 1px 1px rgba(0, 0, 0, 0.2);
-    -webkit-transition-duration: 0.2s;
-    -moz-transition-duration: 0.2s;
     transition-duration: 0.2s;
-    -webkit-user-select:none;
-    -moz-user-select:none;
-    -ms-user-select:none;
     user-select:none;
+    margin: 10px;
+    margin-top: 30px;
   }
   #save:hover {
     background: #356094;
@@ -75,8 +219,6 @@
     text-decoration: none;
   }
   #save:active {
-      -webkit-box-shadow: inset 0 1px 4px rgba(0, 0, 0, 0.6);
-      -moz-box-shadow: inset 0 1px 4px rgba(0, 0, 0, 0.6);
       box-shadow: inset 0 1px 4px rgba(0, 0, 0, 0.6);
       background: #2E5481;
       border: solid 1px #203E5F;
@@ -95,7 +237,7 @@
           ingredientLists: [
             {name: "Koostis", ingredientLines: [{amount: null, unit: null, ingredient: null}]}
           ],
-          categories: []
+          categories: [{}]
         }
       };
     },
@@ -129,7 +271,7 @@
         this.remove(this.recipe.ingredientLists, list);
       },
       deleteRow: function (row, list) {
-        this.remove(list, row);
+        this.remove(list.ingredientLines, row);
       },
       deleteCategoryRow: function (row) {
         this.remove(this.recipe.categories, row);
