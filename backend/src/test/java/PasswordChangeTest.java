fc8d632b00285174af935e4f46b2823f983f66ed
Kaarel <kaarelkangro@hot.ee>
2017-06-22 16:39:57 +0300

    Selenide test for changing password

--- backend/src/test/java/PasswordChangeTest.java
+++ backend/src/test/java/PasswordChangeTest.java
@@ -1,77 +1,73 @@
-/*
- * Created by Kaarel on 21.06.2017.
- */
-import static com.codeborne.selenide.Selenide.*;
 import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.Test;
-import static com.codeborne.selenide.Condition.*;
 import org.openqa.selenium.By;
 
-public class PasswordChangeTest extends SelenideTestsBase {
-    private String newPassword = "tester";
+import static com.codeborne.selenide.Condition.exactTextCaseSensitive;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
 
-    private void setOldPasswordValue(String value) {
-        selectByPlaceholder("Vana parool").setValue(value);
-    }
+public class PasswordChangeTest extends BaseSelenideTest {
+  @Test
+  public void changingPasswordWorks() {
+    openUserPage();
+    emptyFormResponseTest();
+    nullNewPasswordResponseTest();
+    emptyNewPasswordResponseTest();
+    falseOldPasswordResponseTest();
+    successfulPasswordChangeTest();
+  }
 
-    private void submitPasswordForm() {
+  private void emptyFormResponseTest() {
+    submitPasswordForm();
+    $(Selectors.byText("Palun sisesta oma praegune parool.")).shouldBe(visible);
+  }
 
-        $(By.id("save")).click();
-    }
+  private void nullNewPasswordResponseTest() {
+    setOldPasswordValue("WrongPassword");
+    submitPasswordForm();
+    $(Selectors.byText("Palun sisesta uus parool.")).shouldBe(visible);
+  }
 
-    private SelenideElement getNewPasswordField() {
+  private void emptyNewPasswordResponseTest() {
+    setNewPasswordValue("a");
+    getNewPasswordField().sendKeys("\b");
+    submitPasswordForm();
+    $(Selectors.byText("Palun sisesta uus parool.")).shouldBe(visible);
+  }
 
-        return selectByPlaceholder("Uus parool");
-    }
+  private void falseOldPasswordResponseTest() {
+    setNewPasswordValue(newPassword);
+    submitPasswordForm();
+    $(Selectors.byText("Parooli muutmine ebaõnnestus. Kontrolli vana parooli õigsust.")).shouldBe(visible);
+  }
 
-    private void setNewPasswordValue(String value) {
-        selectByPlaceholder("Uus parool").setValue(value);
-        return;
-    }
+  private void successfulPasswordChangeTest() {
+    setOldPasswordValue(testPassword);
+    submitPasswordForm();
+    testPassword = newPassword;
+    $(Selectors.byText("Parool edukalt muudetud!")).shouldBe(visible);
+    $(Selectors.byText("Logi välja")).click();
+    login();
+    $(By.className("logout")).shouldHave(exactTextCaseSensitive(testUsername));
+  }
 
-    private void emptyFormResponseTest() {
-        submitPasswordForm();
-        $(Selectors.byText("Palun sisesta oma praegune parool.")).shouldBe(visible);
-    }
+  private final String newPassword = "tester";
 
-    private void nullNewPasswordResponseTest() {
-        setOldPasswordValue("WrongPassword");
-        submitPasswordForm();
-        $(Selectors.byText("Palun sisesta uus parool.")).shouldBe(visible);
-    }
+  private void setOldPasswordValue(String value) {
+    selectByPlaceholder("Vana parool").setValue(value);
+  }
 
-    private void emptyNewPasswordResponseTest() {
-        setNewPasswordValue("a");
-        getNewPasswordField().sendKeys("\b");
-        submitPasswordForm();
-        $(Selectors.byText("Palun sisesta uus parool.")).shouldBe(visible);
-    }
+  private void submitPasswordForm() {
+    $(By.id("save")).click();
+  }
 
-    private void falseOldPasswordResponseTest() {
-        setNewPasswordValue(newPassword);
-        submitPasswordForm();
-        $(Selectors.byText("Parooli muutmine ebaõnnestus. Kontrolli vana parooli õigsust.")).shouldBe(visible);
-    }
+  private SelenideElement getNewPasswordField() {
+    return selectByPlaceholder("Uus parool");
+  }
 
-    private void successfulPasswordChangeTest() {
-        setOldPasswordValue(testpassword);
-        submitPasswordForm();
-        testpassword = newPassword;
-        $(Selectors.byText("Parool edukalt muudetud!")).shouldBe(visible);
-        $(Selectors.byText("Logi välja")).click();
-        login();
-        $(By.className("logout")).shouldHave(exactTextCaseSensitive(testusername));
-    }
+  private void setNewPasswordValue(String value) {
+    getNewPasswordField().setValue(value);
+  }
 
-    @Test
-    public void changingPasswordWorks() {
-        setUpTest();
-        openUserPage();
-        emptyFormResponseTest();
-        nullNewPasswordResponseTest();
-        emptyNewPasswordResponseTest();
-        falseOldPasswordResponseTest();
-        successfulPasswordChangeTest();
-    }
 }
