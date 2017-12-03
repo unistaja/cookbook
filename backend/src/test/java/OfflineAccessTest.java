3d846ce19d851d6298682592a778916c4e853468
Kaarel <kaarelkangro@hot.ee>
2017-12-03 14:58:00 +0200

    Added a test for offline access

---
+++ backend/src/test/java/OfflineAccessTest.java
@@ -0,0 +1,26 @@
+import com.codeborne.selenide.Selectors;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import static com.codeborne.selenide.Selenide.*;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.WebDriverRunner.url;
+
+public class OfflineAccessTest extends BaseSelenideTest {
+  @Test
+  public void testOfflineAccess() {
+    $(Selectors.byText("Logi välja")).click();
+    $(Selectors.byText("Logi välja")).shouldNotBe(visible);
+    $(By.id("navbar")).shouldNotBe(visible);
+    testPage("recipelist");
+    testPage("addrecipe");
+    testPage("user");
+  }
+
+  private void testPage(String page) {
+    open("/#/"+page);
+    assert url().equals(baseUrl + "login#/"+page);
+    $(By.id("app")).shouldNotBe(visible);
+  }
+}
