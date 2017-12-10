bca8509583a1d51ef4b5611e81b627634800356f
Kaarel <kaarelkangro@hot.ee>
2017-12-10 11:35:05 +0200

    removed unnecessary import

--- backend/src/main/java/ee/cookbook/schedule/SchedulePeriodicImageDeletion.java
+++ backend/src/main/java/ee/cookbook/schedule/SchedulePeriodicImageDeletion.java
@@ -2,7 +2,6 @@ package ee.cookbook.schedule;
 
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import java.io.File;
@@ -11,9 +10,8 @@ import java.io.File;
 public class SchedulePeriodicImageDeletion {
   @Value("${imageFolder}")
   private String imageFolder;
-  @Scheduled(cron = "0 * 12 * * ?")
+  @Scheduled(cron = "0 0 3 * * ?")
   public void deleteImages() {
-    System.out.println("SCHEDULE!");
     final File dir = new File(imageFolder + "temp/");
     if (dir.exists()) {
       final File[] images = dir.listFiles();
