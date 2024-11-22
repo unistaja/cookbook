8cc9a6ccaf5b58e742c101707a3eda55c5d54f16
unistaja <8427397+unistaja@users.noreply.github.com>
2024-11-22 13:53:52 +0200

    add possibility to delete parsed ingredient rows on ingredient addition

--- frontend/src/components/AddRecipeModal.jsx
+++ frontend/src/components/AddRecipeModal.jsx
@@ -3,6 +3,7 @@ import Autocomplete from '@mui/material/Autocomplete';
 import Button from '@mui/material/Button';
 import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
+import IconButton from '@mui/material/IconButton';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -11,6 +12,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
+import ClearIcon from '@mui/icons-material/Clear';
 
 export default function AddRecipeModal({ingredientLists, onUpdateIngredientLists, autoFillData, open, onClose, onSubmit}) {
   const updateIngredientField = useCallback((newValue, fieldName, listIdx, ingredientIdx, altIngredientIdx) => {
@@ -23,6 +25,17 @@ export default function AddRecipeModal({ingredientLists, onUpdateIngredientLists
     onUpdateIngredientLists(newLists);
   }, [ingredientLists, onUpdateIngredientLists]);
 
+  const deleteIngredient = useCallback((listIdx, ingredientIdx, altIngredientIdx) => {
+    const newLists = JSON.parse(JSON.stringify(ingredientLists)); // make deep copy
+    if (altIngredientIdx !== undefined) {
+      newLists[listIdx].ingredientLines[ingredientIdx].alternateLines.splice(altIngredientIdx, 1);
+    } else {
+      newLists[listIdx].ingredientLines.splice(ingredientIdx, 1);
+    }
+    onUpdateIngredientLists(newLists);
+    
+  }, [ingredientLists, onUpdateIngredientLists])
+
   return (
     <Dialog onClose={onClose} open={open} fullWidth={true} maxWidth="md">
       <DialogTitle>Kontrolli ja vajadusel muuda ühikuid ja otsingukoostisosi</DialogTitle>
@@ -34,11 +47,12 @@ export default function AddRecipeModal({ingredientLists, onUpdateIngredientLists
              <TableCell>Ühik</TableCell>
              <TableCell>Koostisosa</TableCell>
              <TableCell>Otsingukoostisosa</TableCell>
+             <TableCell></TableCell>
            </TableRow>
          </TableHead>
          {ingredientLists.map((list, listIdx) => (
            <TableBody key={`list-${listIdx}`}>
-             {ingredientLists.length > 1 && <TableRow key={`list-heading-${listIdx}`}><TableCell colSpan="4">{list.name}</TableCell></TableRow>}
+             {ingredientLists.length > 1 && <TableRow key={`list-heading-${listIdx}`}><TableCell colSpan="5">{list.name}</TableCell></TableRow>}
              {list.ingredientLines.map((ingredient, ingredientIdx) => (
                <Fragment key={`list-${listIdx}-ingredientline-${ingredientIdx}`}>
                <TableRow>
@@ -66,6 +80,7 @@ export default function AddRecipeModal({ingredientLists, onUpdateIngredientLists
                      renderInput={(params) => <TextField {...params}/>}
                    />
                  </TableCell>
+                 <TableCell><IconButton onClick={() => deleteIngredient(listIdx, ingredientIdx)}><ClearIcon/></IconButton></TableCell>
                </TableRow>
                {ingredient.alternateLines.map((altIngredient, altIdx) => (
                  <TableRow>
@@ -93,6 +108,7 @@ export default function AddRecipeModal({ingredientLists, onUpdateIngredientLists
                        renderInput={(params) => <TextField {...params}/>}
                      />
                    </TableCell>
+                   <TableCell><IconButton onClick={() => deleteIngredient(listIdx, ingredientIdx, altIdx)}><ClearIcon/></IconButton></TableCell>
                  </TableRow>
                ))}
                </Fragment>
